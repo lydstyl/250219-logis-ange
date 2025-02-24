95970279338f97857412fa47dd076ab3646ce3c3
Gabriel Brun <lydstyl@gmail.com>
2025-02-24 19:26:13 +0100

    Change contact page.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,17 @@
 export default function Contact() {
   return (
     <div>
-      <p className='text-center text-xl font-bold text-green-600'>
+      <p className='text-center text-xl font-bold text-green-600 mb-6'>
         <a
           href='https://docs.google.com/forms/d/1fnmE1A-iPCkiiUdbFCFF7hRCDCpWVG7W7S8CtUj8fxg/edit'
           target='_blank'
-          // rel='noreferrer'
+          rel='noopener noreferrer'
         >
           Ne ratez pas cette opportunité ! <br />
           Cliquer ici pour aller plus loin
         </a>
       </p>
+      <p className='text-center'>Ou contacter moi sur LYDSTYL@GMAIL.COM</p>
     </div>
   )
 }
